dddbf8547bd86410d3b3f479eb900d48a21ca1df
pvy-it-tdh <phucvy107@gmail.com>
2024-06-10 14:57:31 +0700

    complete Header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,41 @@
+import { GrSearch } from "react-icons/gr";
+import { FaCircleUser } from "react-icons/fa6";
+import { FaShoppingCart } from "react-icons/fa";
 const Header = () => {
   return (
     <header className="h-16 shadow-md">
       <div className="h-full container mx-auto flex items-center px-4 justify-between">
         <div>Logo</div>
-        <div>
-          <input type="text" placeholder="search product here.." />
+        <div className="hidden lg:flex items-center w-full justify-between max-w-sm border rounded-full focus-within:shadow-md pl-2">
+          <input
+            type="text"
+            placeholder="search product here.."
+            className="w-full outline-none pl-2"
+          />
+          <div className="text-lg min-w-[50px] h-8 bg-red-600 flex items-center justify-center rounded-r-full">
+            <GrSearch />
+          </div>
         </div>
-        <div>User icon and Cart</div>
+        <div className="flex items-center gap-7">
+          <div className="text-3xl cursor-pointer">
+            <FaCircleUser />
+          </div>
+
+          <div className="text-2xl relative">
+            <span>
+              <FaShoppingCart />
+            </span>
+            <div className="bg-red-600 text-white w- p-1 rounded-full flex items-center justify-center absolute -top-2 -right-3">
+              <p className="text-sm">0</p>
+            </div>
+          </div>
+
+          <div>
+            <button className="px-3 py-1 rounded-full bg-red-600 text-white hover:bg-red-700">
+              Login
+            </button>
+          </div>
+        </div>
       </div>
     </header>
   );
