580825ccf31809afa49701067092ccd394d03bd8
pvy-it-tdh <phucvy107@gmail.com>
2024-06-11 17:17:07 +0700

    add backend

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const connectDB = require("./config/db");
+const router = require("./routes/index");
+
+const app = express();
+app.use(cors());
+app.use("/api", router);
+const PORT = 8080 || process.env.PORT;
+connectDB().then(
+  app.listen(PORT, () => {
+    console.log("Server is running ");
+    console.log("Connect DB");
+  })
+);
