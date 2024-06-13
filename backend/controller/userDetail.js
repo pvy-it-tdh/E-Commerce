f0ca8633338fbacfc9d31424260224c1f5792b75
pvy-it-tdh <phucvy107@gmail.com>
2024-06-13 20:21:17 +0700

    complete UserDetail

--- backend/controller/userDetail.js
+++ backend/controller/userDetail.js
@@ -1,6 +1,14 @@
+const userModel = require("../model/User");
 async function userDetailController(req, res) {
   try {
     console.log("userID", req.userId);
+    const user = await userModel.findById(req.userId);
+    res.status(200).json({
+      data: user,
+      error: false,
+      success: true,
+      message: "User details",
+    });
   } catch (error) {
     res.status(400).json({
       message: error.message || error,
